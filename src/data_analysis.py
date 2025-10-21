6d27140dfa1ab6d406b9eb878e403d3ff7c3bfba
Tara <189487620+t25ara@users.noreply.github.com>
2025-10-20 18:02:51 -0700

    finsihed data_analysis.py

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -22,24 +22,26 @@ def count_average_grade(grades):
     return sum(grades)/len(grades)
 
 def count_math_students(students):
-    math_students = 0 
+    n_math_students = 0 
     for student in students:
-        if student['subject'] == "Math":
-            math_students = math_students + 1 
-    return(math_students)
+        if student['subject'].strip().lower() == "math":
+            n_math_students = n_math_students + 1 
+    return(n_math_students)
 
 def generate_report(students, grades, average, math_students):
-    print( "____REPORT_____")
+    print("____REPORT_____")
     print("Student Data \n", students)
     print("Grades\n", grades)
     print(f"Average Grade:\n {average:.1f}")
     print("Number of Math Students\n", math_students)
+    report = (f"____REPORT_____\nAverage Grade:{average:.1f}\nNumber of Math Students:\n{math_students}")
+    return(report)
 
 
 def save_report(report, filename):
     with open(filename, 'w') as file:
         file.write(str(report))  # write the string directly
-    print(f"Detailed report saved to {filename}")
+    print(f"report saved")
     
 
 def main(myfile):
@@ -47,6 +49,7 @@ def main(myfile):
     grades = [student['grade'] for student in students]
     average = count_average_grade(grades)
     math_students = count_math_students(students)
+    filename = "student_analysis.txt"
     report = generate_report(students, grades, average, math_students)
     save_report(report, "output/analysis_report.txt")
     print("analysis complete results saved to analysis_report.txt")
